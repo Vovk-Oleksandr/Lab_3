3d211af99d061a3f68a6b216db864ae2c55cde04
volyn <volyn@DESKTOP-AMLADTD>
2024-12-08 01:04:18 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    // Базовий клас "Живий організм"
+    abstract class LivingOrganism
+    {
+        public double Energy { get; set; }
+        public int Age { get; set; }
+        public double Size { get; set; }
+
+        public LivingOrganism(double energy, int age, double size)
+        {
+            Energy = energy;
+            Age = age;
+            Size = size;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Енергія: {Energy}, Вік: {Age}, Розмір: {Size}");
+        }
+    }
+
+    // Інтерфейс для розмноження
+    interface IReproducible
+    {
+        void Reproduce();
+    }
+
+    // Інтерфейс для хижаків
+    interface IPredator
+    {
+        void Hunt(LivingOrganism prey);
+    }
+
+    // Клас "Тварина"
+    class Animal : LivingOrganism, IReproducible, IPredator
+    {
+        public string Species { get; set; }
+
+        public Animal(double energy, int age, double size, string species) : base(energy, age, size)
+        {
+            Species = species;
+        }
+
+        public void Reproduce()
+        {
+            Console.WriteLine($"{Species} розмножується...");
+        }
+
+        public void Hunt(LivingOrganism prey)
+        {
+            Console.WriteLine($"{Species} полює на жертву...");
+            Energy += prey.Size * 0.5; // Енергія збільшується
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Вид: {Species}");
+        }
+    }
+
+    // Клас "Рослина"
+    class Plant : LivingOrganism, IReproducible
+    {
+        public string PlantType { get; set; }
+
+        public Plant(double energy, int age, double size, string plantType) : base(energy, age, size)
+        {
+            PlantType = plantType;
+        }
+
+        public void Reproduce()
+        {
+            Console.WriteLine($"Рослина типу {PlantType} розмножується...");
+        }
+    }
+
+    // Клас "Мікроорганізм"
+    class Microorganism : LivingOrganism
+    {
+        public string Species { get; set; }
+
+        public Microorganism(double energy, int age, double size, string species) : base(energy, age, size)
+        {
+            Species = species;
+        }
+    }
+
+    // Клас "Екосистема"
+    class Ecosystem
+    {
+        private List<LivingOrganism> organisms = new List<LivingOrganism>();
+
+        public void AddOrganism(LivingOrganism organism)
+        {
+            organisms.Add(organism);
+        }
+
+        public void Simulate()
+        {
+            Console.WriteLine("Симуляція екосистеми почалася...");
+            foreach (var organism in organisms)
+            {
+                organism.DisplayInfo();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ecosystem ecosystem = new Ecosystem();
+
+            Animal lion = new Animal(100, 5, 50, "Лев");
+            Plant tree = new Plant(50, 10, 100, "Дуб");
+            Microorganism bacteria = new Microorganism(10, 1, 0.5, "Бактерія");
+
+            ecosystem.AddOrganism(lion);
+            ecosystem.AddOrganism(tree);
+            ecosystem.AddOrganism(bacteria);
+
+            lion.Hunt(bacteria);
+            lion.Reproduce();
+
+            tree.Reproduce();
+
+            ecosystem.Simulate();
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Інтерфейс для з'єднання
+public interface IConnectable
+{
+    void Connect(IConnectable device);
+    void Disconnect();
+    void SendData(string data, IConnectable recipient);
+}
+
+// Базовий клас Комп'ютер
+public abstract class Computer : IConnectable
+{
+    public string IpAddress { get; set; }
+    public int Power { get; set; }
+    public string OperatingSystem { get; set; }
+
+    protected Computer(string ipAddress, int power, string operatingSystem)
+    {
+        IpAddress = ipAddress;
+        Power = power;
+        OperatingSystem = operatingSystem;
+    }
+
+    public abstract void Connect(IConnectable device);
+    public abstract void Disconnect();
+    public abstract void SendData(string data, IConnectable recipient);
+
+    public override string ToString()
+    {
+        return $"{GetType().Name} (IP: {IpAddress}, OS: {OperatingSystem})";
+    }
+}
+
+// Клас Сервер
+public class Server : Computer
+{
+    public int StorageCapacity { get; set; }
+
+    public Server(string ipAddress, int power, string operatingSystem, int storageCapacity)
+        : base(ipAddress, power, operatingSystem)
+    {
+        StorageCapacity = storageCapacity;
+    }
+
+    public override void Connect(IConnectable device)
+    {
+        Console.WriteLine($"{this} підключився до {device}.");
+    }
+
+    public override void Disconnect()
+    {
+        Console.WriteLine($"{this} відключився.");
+    }
+
+    public override void SendData(string data, IConnectable recipient)
+    {
+        Console.WriteLine($"{this} відправив дані '{data}' до {recipient}.");
+    }
+}
+
+// Клас Робоча станція
+public class Workstation : Computer
+{
+    public string User { get; set; }
+
+    public Workstation(string ipAddress, int power, string operatingSystem, string user)
+        : base(ipAddress, power, operatingSystem)
+    {
+        User = user;
+    }
+
+    public override void Connect(IConnectable device)
+    {
+        Console.WriteLine($"{this} підключився до {device}.");
+    }
+
+    public override void Disconnect()
+    {
+        Console.WriteLine($"{this} відключився.");
+    }
+
+    public override void SendData(string data, IConnectable recipient)
+    {
+        Console.WriteLine($"{this} відправив дані '{data}' до {recipient}.");
+    }
+}
+
+// Клас Маршрутизатор
+public class Router : Computer
+{
+    public int MaxConnections { get; set; }
+
+    public Router(string ipAddress, int power, string operatingSystem, int maxConnections)
+        : base(ipAddress, power, operatingSystem)
+    {
+        MaxConnections = maxConnections;
+    }
+
+    public override void Connect(IConnectable device)
+    {
+        Console.WriteLine($"{this} підключився до {device}.");
+    }
+
+    public override void Disconnect()
+    {
+        Console.WriteLine($"{this} відключився.");
+    }
+
+    public override void SendData(string data, IConnectable recipient)
+    {
+        Console.WriteLine($"{this} відправив дані '{data}' до {recipient}.");
+    }
+}
+
+// Клас Мережа
+public class Network
+{
+    private List<IConnectable> Devices = new List<IConnectable>();
+
+    public void AddDevice(IConnectable device)
+    {
+        Devices.Add(device);
+        Console.WriteLine($"Пристрій {device} доданий до мережі.");
+    }
+
+    public void RemoveDevice(IConnectable device)
+    {
+        Devices.Remove(device);
+        Console.WriteLine($"Пристрій {device} видалений з мережі.");
+    }
+
+    public void SendData(string data, IConnectable sender, IConnectable recipient)
+    {
+        Console.WriteLine($"Мережа: {sender} відправляє дані '{data}' до {recipient}.");
+        sender.SendData(data, recipient);
+    }
+}
+
+// Тестування
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створення пристроїв
+        var server = new Server("192.144.1.1", 500, "Linux", 1000);
+        var workstation = new Workstation("192.168.1.2", 300, "Windows", "User1");
+        var router = new Router("192.144.1.254", 100, "CiscoOS", 10);
+
+        // Створення мережі
+        var network = new Network();
+
+        // Додавання пристроїв до мережі
+        network.AddDevice(server);
+        network.AddDevice(workstation);
+        network.AddDevice(router);
+
+        // Передача даних
+        network.SendData("Hello, World!", workstation, server);
+
+        // Відключення пристрою
+        workstation.Disconnect();
+    }
+}
